3f1fbc48c10d5d4c18bb88447943d05ed5e1ff7c
VladSkopenko <skopirka2k17@gmail.com>
2025-06-26 17:19:38 +0300

    heck add

---
+++ check_name.py
@@ -0,0 +1,17 @@
+from pdfrw import PdfReader
+
+def list_pdf_form_fields(pdf_path: str):
+    pdf = PdfReader(pdf_path)
+    field_names = set()
+    for page in pdf.pages:
+        annotations = page.get('/Annots') or []
+        for annotation in annotations:
+            field_name = annotation.get('/T')
+            if field_name:
+                field_names.add(field_name.to_unicode().strip('()'))
+    return field_names
+
+# Использование:
+fields = list_pdf_form_fields('inbulk_ttn.pdf')
+for name in fields:
+    print(name)
